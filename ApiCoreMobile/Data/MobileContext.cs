c9fe66e8e3b5676a3fd555afd405f51f04b29153
ahmed <ahmed@DESKTOP-GAFE4G5>
2022-09-02 15:49:06 +0200

    Add Data To DB

--- ApiCoreMobile/Data/MobileContext.cs
+++ ApiCoreMobile/Data/MobileContext.cs
@@ -6,8 +6,46 @@ namespace ApiCoreMobile.Data
     {
         public MobileContext(DbContextOptions options) : base() { }
 
-        public  DbSet<Mobiles> mobiles { get; set; }
+        public DbSet<Mobiles> mobiles { get; set; }
         public DbSet<Category> categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Mobiles>().HasData(
+                new Mobiles
+                {
+                    Id = 1,
+                    Name = "Iphone 6 ",
+                    Price = 6500,
+                    CategoryId = 1
+
+                },
+                new Mobiles
+                {
+                    Id = 2,
+                    Name = "Iphone 7 ",
+                    Price = 7500,
+                    CategoryId = 1
+                },
+                  new Mobiles
+                  {
+                      Id = 3,
+                      Name = "Iphone 8 ",
+                      Price = 9500,
+                      CategoryId = 1
+
+                  }
+                );
+            modelBuilder.Entity<Category>().HasData(
+                        new Category
+                        {
+                            Id = 1,
+                            Name = "Iphone"
+                        },
+                        new Category { Id = 2, Name = "Samsung" },
+                        new Category { Id = 3, Name = "Oppo" }
+                );
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             var configuration = new ConfigurationBuilder()
---
+++ ApiCoreMobile/Migrations/20220902134822_AddData.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiCoreMobile.Migrations
+{
+    public partial class AddData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "Iphone" });
+
+            migrationBuilder.InsertData(
+                table: "categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2, "Samsung" });
+
+            migrationBuilder.InsertData(
+                table: "categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 3, "Oppo" });
+
+            migrationBuilder.InsertData(
+                table: "mobiles",
+                columns: new[] { "Id", "CategoryId", "Name", "Price" },
+                values: new object[] { 1, 1, "Iphone 6 ", 6500.0 });
+
+            migrationBuilder.InsertData(
+                table: "mobiles",
+                columns: new[] { "Id", "CategoryId", "Name", "Price" },
+                values: new object[] { 2, 1, "Iphone 7 ", 7500.0 });
+
+            migrationBuilder.InsertData(
+                table: "mobiles",
+                columns: new[] { "Id", "CategoryId", "Name", "Price" },
+                values: new object[] { 3, 1, "Iphone 8 ", 9500.0 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "mobiles",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "mobiles",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "mobiles",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
